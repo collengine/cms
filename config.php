a9f33bd973a8aff07bfd3f836c30fa36f61d2db4
collengine <37255906+collengine@users.noreply.github.com>
2018-03-11 04:32:58 +0000

    Update config.php
    revised the host and pass

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 ini_set( "display_errors", true );
 date_default_timezone_set( "Australia/Sydney" );  // http://www.php.net/manual/en/timezones.phplocalhost
-define( "DB_DSN", "sqlsrv:server = tcp:cmsbooksonline.database.windows.net,1433; Database = cms" );
-define( "DB_USERNAME", "onekoetch" );
+define( "DB_DSN", "mysql:host=bookfair-mysqldbserver.mysql.database.azure.com; dbname=cms" );
+define( "DB_USERNAME", "peterkoech@bookfair-mysqldbserver" );// "mysql:host=localhost;dbname=cms" 
 define( "DB_PASSWORD", "#Robert15" );
 define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
