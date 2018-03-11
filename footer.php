c4e58b7c2034b87580ea1c359c5cc9bb5aca791a
collengine <37255906+collengine@users.noreply.github.com>
2018-03-11 08:43:39 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-        Widget News &copy; 2011. All rights reserved. <a href="admin.php">Site Admin</a>
+        Books Online &copy; 2018. All rights reserved. <a href="admin.php">Site Admin</a>
       </div>
  
     </div>
