ac1b829bb734fee7f3a3e06d2907744873ff8d8c
collengine <37255906+collengine@users.noreply.github.com>
2018-03-11 07:36:45 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php echo htmlspecialchars( $results['pageTitle'] )?></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body>
+    <div id="container">
+ 
+      <a href="."><img style = "width: 300px; height: 200px;"id="logo" src="images/logo.jpg" alt="Widget News" /></a>
+      
